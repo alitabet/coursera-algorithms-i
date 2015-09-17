3a021d5bbae39dbad49d1bf4bc8d5e3fe397f7d1
Ali Thabet <alitabet@gmail.com>
2015-09-17 16:56:31 +0300

    Implement sample() method

--- AlgorithmsI/src/RandomizedQueue.java
+++ AlgorithmsI/src/RandomizedQueue.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -18,7 +21,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
 
     /**
-     * Initializes an empty queue.
+     * Initializes an empty randomized queue.
      */
     public RandomizedQueue() {
         // cast needed since no generic array creation in Java
@@ -26,21 +29,33 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     /**
-     * Is this stack empty?
-     * @return true if this stack is empty; false otherwise
+     * Is this queue empty?
+     * @return true if this queue is empty; false otherwise
      */
     public boolean isEmpty() {
         return N == 0;
     }
 
     /**
-     * Returns the number of items in the stack.
-     * @return the number of items in the stack
+     * Returns the number of items in this queue.
+     *
+     * @return the number of items in this queue
      */
     public int size() {
         return N;
     }
 
+    /**
+     * Helper function to check if item is null
+     *
+     * @param item
+     * @throws NullPointerException if item is NULL
+     */
+    private void checkItem(Item item) {
+        if (item.equals(null)) {
+            throw new java.lang.NullPointerException("Item provided is NULL");
+        }
+    }
 
     // resize the underlying array holding the elements
     private void resize(int capacity) {
@@ -53,21 +68,25 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     /**
-     * Adds the item to this stack.
+     * Adds the item to this queue.
+     *
      * @param item the item to add
+     * @throws NullPointerException if item is NULL
      */
-    public void push(Item item) {
+    public void enqueue(Item item) {
+        checkItem(item);
         if (N == a.length) resize(2*a.length);    // double size of array if necessary
         a[N++] = item;                            // add item
     }
 
     /**
-     * Removes and returns the item most recently added to this stack.
-     * @return the item most recently added
-     * @throws java.util.NoSuchElementException if this stack is empty
+     * Removes and returns a random item on this queue
+     *
+     * @return a randomly selected item on this queue
+     * @throws java.util.NoSuchElementException if this queue is empty
      */
-    public Item pop() {
-        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
         Item item = a[N-1];
         a[N-1] = null;                              // to avoid loitering
         N--;
@@ -78,13 +97,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
 
     /**
-     * Returns (but does not remove) the item most recently added to this stack.
-     * @return the item most recently added to this stack
+     * Returns (but does not remove) a random item on this queue
+     *
+     * @return a random item on this queue
      * @throws java.util.NoSuchElementException if this stack is empty
      */
-    public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
-        return a[N-1];
+    public Item sample() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return a[StdRandom.uniform(N)];
     }
 
     @Override
@@ -109,5 +129,30 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         public Item next() {
             return null;
         }
+    }
+
+    /**
+     * Unit tests the <tt>Deque</tt> data type.
+     */
+    public static void main(String[] args) {
+        Integer[] temp = new Integer[10];
+
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = StdRandom.uniform(100);
+        }
+
+        for (int i : temp) {
+            StdOut.print(i + " ");
+        }
+        StdOut.print("\n");
+
+        int idx = StdRandom.uniform(temp.length);
+        StdOut.println("Random index = " + idx);
+        temp[5] = null;
+        StdOut.println("Length = " + temp.length);
+        for (int i : temp) {
+            StdOut.print(i + " ");
+        }
+        StdOut.print("\n");
     }
 }
